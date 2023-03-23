fdca90c12c2fec1031b8426aa8e065b09c8c0c06
lostie21yo <leon.coxsw@gmail.com>
2023-03-23 12:42:31 +0400

    samara parsed

--- script.py
+++ script.py
@@ -5,24 +5,24 @@ import pandas as pd
 import os
 import time
 
-city = "г. Ставрополь, "
-file_dir = os.path.split(__file__)[0].replace('\\', '/')
-df = pd.read_csv(f"{file_dir}/Stavropol.csv", delimiter='\t', encoding='utf-16') # old
-# df = pd.DataFrame(columns=('Address', 'Name', 'Number')) # new
-
+city = "Samara"
 links = (
-    ("*", "https://telspravki.info/rossiya/stavropolskij-kraj/oblastnoj-tsyentr/stavropol?serchStreet=%21"), # All
-    ("К", "https://telspravki.info/rossiya/stavropolskij-kraj/oblastnoj-tsyentr/stavropol?serchStreet=%CA"), # K
-    ("П", "https://telspravki.info/rossiya/stavropolskij-kraj/oblastnoj-tsyentr/stavropol?serchStreet=%CF"), # П
-    ("С", "https://telspravki.info/rossiya/stavropolskij-kraj/oblastnoj-tsyentr/stavropol?serchStreet=%D1"), # С
+    "https://telspravki.info/rossiya/samarskaya-oblast/oblastnoj-tsyentr/samara?serchStreet=%21", # all
+    "https://telspravki.info/rossiya/samarskaya-oblast/oblastnoj-tsyentr/samara?serchStreet=%CA", # К
+    "https://telspravki.info/rossiya/samarskaya-oblast/oblastnoj-tsyentr/samara?serchStreet=%D1" # С
 )
-
-exception_symbol = ("К", "П", "С") # исключения (обработать вручную)
+exception_symbol = ("К", "С") # исключения (обработать вручную)
 alphabet = "0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюя".upper() # Общий алфавит
 done = "0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюя".upper() # сохранение завершенных букв алфавита (за исключением отдельных букв в exception symbol)
 
+file_dir = os.path.split(__file__)[0].replace('\\', '/')
+try:
+    df = pd.read_csv(f"{file_dir}/{city}.csv", delimiter='\t', encoding='utf-16') # old
+except:
+    df = pd.DataFrame(columns=('Address', 'Name', 'Number')) # new
+
 def parse():
-    main_r = requests.get(links[3][1])
+    main_r = requests.get(links[2])
     html = BS(main_r.content, 'html.parser')
 
     # Страница города с улицами
@@ -32,7 +32,7 @@ def parse():
         for street in streets:
             # if street.text.startswith("Кол"):
             if street.text[0] == "С":
-            # if street.text[0] not in done and street.text[0] not in exception_symbol: 
+            # if street.text[0] in alphabet and street.text[0] not in done and street.text[0] not in exception_symbol: 
                 print(f"=== {street.text} ===")
                 street_r = requests.get(f"https:{street.get('href')}")
                 html1 = BS(street_r.content, 'html.parser')
@@ -71,18 +71,17 @@ def parse():
                                     print(f"{count}.   {address}, {fio}, {number}")
                             except:
                                 pass
-                        #df.drop_duplicates().to_csv(f'{file_dir}/stavropol.csv', sep='\t', index= False, encoding="utf-16")
                     
-                df.to_csv(f'{file_dir}/Stavropol.csv', sep='\t', index= False, encoding="utf-16")  
+                df.to_csv(f'{file_dir}/{city}.csv', sep='\t', index= False, encoding="utf-16")  
 
 start = time.time()            
 parse()
 finish = time.time() - start
 
-print(f"Затраченное время на парсинг {finish//60} мин {round(finish%60)} сек")
+print(f"Затраченное время {finish//60} мин {round(finish%60)} сек")
 
-df = df.drop_duplicates()
+
 df = df.sort_values(by='Address', ascending=True)
 for row in df.values:
     row[2] = " " + row[2]
-df.to_csv(f'{file_dir}/Stavropol.csv', sep='\t', index= False, encoding="utf-16")
+df.to_csv(f'{file_dir}/{city}.csv', sep='\t', index= False, encoding="utf-16")
--- tool.py
+++ tool.py
@@ -1,9 +1,30 @@
 import pandas as pd
 import os
+from collections import Counter
+
+city = "Samara"
 
 file_dir = os.path.split(__file__)[0]
-df = pd.read_csv(f"{file_dir}/Stavropol.csv", delimiter='\t', encoding='utf-16') # old
-for row in df.values:
-    row[2] = " " + row[2]
 
-df.to_csv(f'{file_dir}/Stavropol_t.csv', sep='\t', index= False, encoding="utf-16")
+df = pd.read_csv(f"{file_dir}/{city}.csv", delimiter='\t', encoding='utf-16') # old
+
+# добавление пробела к номеру
+def space_adding(df):
+    for row in df.values:
+        row[2] = " " + row[2]
+
+# подсчет кол-ва вхождений каждого элемента в список
+def duplicate_counting(df, limit):
+    array = []
+    for i in range(len(df.values)):
+        array.append(f'{df.values[i][0]} {df.values[i][1]} {df.values[i][2]}')
+
+    c = Counter(array).most_common(limit)
+    for elem in c:
+        print(elem)
+
+space_adding(df)
+
+df = df.drop_duplicates()
+
+df.to_csv(f'{file_dir}/{city}_t.csv', sep='\t', index= False, encoding="utf-16")
